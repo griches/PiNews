4a06502435ae59f84dd8c5b7e79daea65d6f892a
Gary Riches and Lewis Mcgrath <griches@ford.com>
2019-07-30 13:46:53 +0100

    Removed un-needed comma

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.0.0"),
-        .package(url: "https://github.com/griches/HD44780CharacterLCD.swift.git", .branch("master")),
+        .package(url: "https://github.com/griches/HD44780CharacterLCD.swift.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
